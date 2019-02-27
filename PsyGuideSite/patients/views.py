2cbba880602f1efced5f64da76a7790aa882f75e
Tonia Schlick <tonia.schlick@gmail.com>
2019-02-26 22:40:34 -0700

    add patient form

--- PsyGuideSite/patients/views.py
+++ PsyGuideSite/patients/views.py
@@ -4,10 +4,16 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from .models import Patient
 
 import patients.QuestionaireLoader as QuestionaireLoader
 
 # Create your views here.
+
+def patient_create_view(request):
+        form = PatientForm(request.POST or None)
+        if form.is_valid():
+                form.save
 
 def index(request):
     return render (request, 'patients/index.html', {'questionaires': range(2)})
