1762d959426cd18cd78bd86087cdcad012bedede
puddinbustra <43453214+puddinbustra@users.noreply.github.com>
2019-03-01 16:10:01 -0700

    Adding a test
    Adding test to test the function I just added, to ensure Travis is working like it's supposed to.

--- tests/simple_test example.py
+++ tests/simple_test example.py
@@ -1,7 +1,10 @@
 import pytest
+from prescriber.tests import travisTest
 #from file import function, for example: from prescriber.admin import adminFunction
 
+assert(travisTest(10) == 10)
 
-#A dummy template for how to format tests
-def simpleTest():
-    assert(someFunction((myInput) == functionOutput))
+##A dummy template for how to format tests
+# def simpleTest():
+#     assert(someFunction((myInput) == functionOutput))
+
