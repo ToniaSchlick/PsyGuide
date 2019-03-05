70538e2296689b7bc497c89ccabee144c2265cbd
kinkalo <kvrosevelt@gmail.com>
2019-03-04 22:57:18 -0700

    Travis progress
    Updated names of test files and functions to include 'test_', which is needed.
    Updated comments and data in the test file.
    Removed __init_.py, which seems unnecessary.
    Simplified and renamed the code to be tested.

---
+++ tests/test_example.py
@@ -0,0 +1,14 @@
+
+
+# from file import function, for example: from prescriber.admin import adminFunction
+from prescriber.exampleTest import aFunction
+
+
+# Note: The name of this file must start with test_, as well as the test functions. This is only because of pytest.
+## A dummy template for how to format tests
+# def test_aFunction():
+#     assert(someFunction((myInput) == functionOutput))
+
+
+def test_example(someInput):
+    assert(travisTest(10) == 10)
