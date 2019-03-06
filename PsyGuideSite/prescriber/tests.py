ede9112c759e366f48db4c5973d57a84ca21ee27
puddinbustra <43453214+puddinbustra@users.noreply.github.com>
2019-03-05 20:18:25 -0700

    Update tests.py
    Removed unused code

--- PsyGuideSite/prescriber/tests.py
+++ PsyGuideSite/prescriber/tests.py
@@ -4,6 +4,3 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
-#This is just to test travis. It's not actually testing anything significant. 
-def travisTest(num == 10):
-  return num
