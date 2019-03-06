3dfd46d21baa11c2ad45b89ce8de6f3abf128b08
Tonia Schlick <tonia.schlick@gmail.com>
2019-03-06 13:01:50 -0700

    made patient accesable from paitiens page

--- PsyGuideSite/patients/views.py
+++ PsyGuideSite/patients/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from .models import patien
 
 import patients.QuestionaireLoader as QuestionaireLoader
 
@@ -20,3 +21,8 @@ def questionaire(request, questionaire):
         return render (request, 'questionaires/questionaire.html',
                 QuestionaireLoader.load_questionaire(questionaire))
 
+
+
+def patient_detail_view(request):
+        obj = patient.object.get(id=1)
+        return render(request, patient/detail.html, {})
