2e0bc759ba1929e014c5a56e3e49b7ba16c13a10
Tonia Schlick <tonia.schlick@gmail.com>
2019-03-11 12:30:41 -0600

    restricted views

--- PsyGuideSite/prescriber/tests.py
+++ PsyGuideSite/prescriber/tests.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.test import TestCase
-
+from django.urls import reverse, resolve
 # Create your tests here.
+
+#test url paths
+class TestUrls:
+
+    def test_index_url(self):
+        path = reverse('index')
+        assert resolve(path).views_name == 'index'
