48b8c1de408d3d26f9c32ace219f3b8f6a27cf6a
Max W <maxsw16@gmail.com>
2019-03-15 23:37:07 -0600

    Small fixes for EC2

--- PsyGuideSite/setup.sh
+++ PsyGuideSite/setup.sh
@@ -4,7 +4,13 @@
 # Utility file to perform all setup and load default data easier
 ###
 
-python manage.py makemigrations
-python manage.py migrate
-python manage.py loaddata user.json
-python manage.py loaddata base.json
+if [ -x "$(command -v python3)" ]; then
+  PY=python3
+else
+  PY=python
+fi
+
+$PY manage.py makemigrations
+$PY manage.py migrate
+$PY manage.py loaddata user.json
+$PY manage.py loaddata base.json
