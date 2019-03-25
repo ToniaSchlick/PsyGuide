89db99ff9cf84de1399e208e1560fb82368deb0f
Tonia Schlick <tonia.schlick@gmail.com>
2019-03-25 09:01:57 -0600

    hyper link to algorithms

--- PsyGuideSite/flowchart/models.py
+++ PsyGuideSite/flowchart/models.py
@@ -1,24 +1,26 @@
 from django.db import models
 
-# Create your models here.
-class Flowchart(models.Model):
-    name = models.CharField(max_length=20)
-    Action = models.ForeignKey(Action, on_delete=models.CASCADE)
-    State = models.ForeignKey(State, on_delete=models.CASCADE)
+# ***** this is not right will be changes
 
-class Action(models.Model):
-    title = models.CharField(max_length=30)
-    stage = models.IntegerField(("stage"))
-    prescription = models.CharField(max_length=30)
-    dose = models.CharField(max_length=30)
-    weeksToNextEval = models.IntegerField(("weeks"))
-    Flowchart = models.ForeignKey(Flowchart, on_delete=models.CASCADE)
+# # Create your models here.
+# class Flowchart(models.Model):
+#     name = models.CharField(max_length=20)
+#     Action = models.ForeignKey(Action, on_delete=models.CASCADE)
+#     State = models.ForeignKey(State, on_delete=models.CASCADE)
 
-class State(models.Model):
-    description = models.CharField(max_length=100)
+# class Action(models.Model):
+#     title = models.CharField(max_length=30)
+#     stage = models.IntegerField(("stage"))
+#     prescription = models.CharField(max_length=30)
+#     dose = models.CharField(max_length=30)
+#     weeksToNextEval = models.IntegerField(("weeks"))
+#     Flowchart = models.ForeignKey(Flowchart, on_delete=models.CASCADE)
 
-class PossProg(models.Model):
-   title = models.CharField(max_length=30)
-   action = models.CharField(max_length=30)
-   weeksToNextEval = models.IntegerField(("weeks"))
+# class State(models.Model):
+#     description = models.CharField(max_length=100)
+
+# class PossProg(models.Model):
+#    title = models.CharField(max_length=30)
+#    action = models.CharField(max_length=30)
+#    weeksToNextEval = models.IntegerField(("weeks"))
    
