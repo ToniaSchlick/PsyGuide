65008d3bfa3a6e5d3a294dc0545880bb660c5085
Tonia Schlick <tonia.schlick@gmail.com>
2019-03-27 20:14:58 -0600

    drawio iframe

--- PsyGuideSite/PsyGuideSite/settings.py
+++ PsyGuideSite/PsyGuideSite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'patient',
     'questionnaire',
     'crispy_forms',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
