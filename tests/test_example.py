eb44fcf77b6b51be73f36ee612190a5ff83c2019
kinkalo <kvrosevelt@gmail.com>
2019-04-02 19:47:36 -0600

    Update test_example.py
    Attempting to make a unit test with django

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,6 +2,9 @@
 
 # from file import function, for example: from prescriber.admin import adminFunction
 from prescriber.exampleTest import aFunction
+from prescriber.views import *
+from django.utils import unittest
+from django.test.client import RequestFactory
 
 
 # Note: The name of this file must start with test_, as well as the test functions. This is only because of pytest.
@@ -12,3 +15,9 @@ from prescriber.exampleTest import aFunction
 
 def test_example():
     assert(aFunction(10) == 10)
+
+# Testing whether the index function completes as it should
+def test_index():
+    f = RequestFactory()
+    request = self.factory.get('/index')
+    print("Going through test_index okay")
