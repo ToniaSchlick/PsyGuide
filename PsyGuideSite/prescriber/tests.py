6ee0851439a5bcab1b8b7857a8cdcb28df9edb04
kinkalo <kvrosevelt@gmail.com>
2019-04-06 20:29:36 -0600

    Added test for basic view
    There's an authenticated, and anonymous test to make sure the code in the view function runs through.

--- PsyGuideSite/prescriber/tests.py
+++ PsyGuideSite/prescriber/tests.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.test import TestCase
+from django.test.client import Client
 from django.urls import reverse, resolve
+from django.test.client import RequestFactory
+from django.contrib.auth.models import AnonymousUser, User
+from prescriber.views import *
+#from django.test.testcases import TestCase
+import unittest
+
 # Create your tests here.
 
 #test url paths
@@ -10,3 +17,14 @@ class TestUrls:
     def test_index_url(self):
         path = reverse('index')
         assert resolve(path).views_name == 'index'
+        assert (response.status_code == 200)
+
+    #Test a successful form registration
+    def test_register(self):
+        pass
+
+
+def main():
+    myTest = TestViewFunctions()
+    myTest.test_index()
+main()
