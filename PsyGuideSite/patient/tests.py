85f0914fe53c0a0d1c5d561dc0d68bbf7acd9858
kinkalo <kvrosevelt@gmail.com>
2019-04-18 21:03:25 -0600

    Refactor and tests
    Refactored some basic tests for less redundant code. Also added basic tests for flowchart and questionnaire.

--- PsyGuideSite/patient/tests.py
+++ PsyGuideSite/patient/tests.py
@@ -21,6 +21,20 @@ class TestViewFunctions(unittest.TestCase):
             self.user.login(username='jacob', password='top_secret')
 
 
+    def test_function(self, name):
+        request = self.factory.get(str(name))
+        request.user = self.user
+        response = eval(name)(request)
+        assert (response.status_code == 200)
+
+
+    def test_function_auth(self, name):
+        request = self.factory.get(str(name))
+        request.user = AnonymousUser()
+        response = eval(name)(request)
+        assert (response.status_code == 200)
+
+
     def test_view_all(self):
         request = self.factory.get('view_all')
         request.user = self.user
@@ -29,19 +43,17 @@ class TestViewFunctions(unittest.TestCase):
 
 
     def test_view(self):
-        request = self.factory.get('view')
-        #print("View request-",request)
-        request.user = self.user
-        response = view(request)
-        assert (response.status_code == 200)
+        self.test_function('view')
 
     # This tests whether code runs through while not being valid, but does not include a form to test submission
     def test_add(self):
+        self.test_function('add')
         #  https://docs.djangoproject.com/en/2.1/topics/testing/tools/#django.test.Client.post
-        request = self.factory.get('add')
-        request.user = self.user
-        response = add(request)
-        assert (response.status_code == 200)
+        # request = self.factory.get('add')
+        # request.user = self.user
+        # response = add(request)
+        # assert (response.status_code == 200)
+
 
     # This is meant to test whether a valid form goes through properly. BUT, need to know first if it's possible to
     # add the form to the request, since that's all that the view takes. Doesn't seem possible.
@@ -58,24 +70,15 @@ class TestViewFunctions(unittest.TestCase):
         # Creates a filled out form with the above data
         pform = PatientForm(data)
 
-        request = self.factory.get('add')
-        request.user = self.user
-        response = add(request)
-        assert (response.status_code == 200)
+        self.test_function('add')
 
 
     def test_delete_no_auth(self):
-        request = self.factory.get('delete')
-        request.user = AnonymousUser()
-        response = delete(request)
-        assert (response.status_code == 200)
+        self.test_function_auth('delete')
 
 
     def test_delete(self):
-        request = self.factory.get('delete')
-        request.user = self.user
-        response = delete(request)
-        assert (response.status_code == 200)
+        self.test_function('delete')
 
 
     def test_edit(self):
--- PsyGuideSite/questionnaire/tests.py
+++ PsyGuideSite/questionnaire/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from questionnaire.views import *
 
-# Create your tests here.
+
+class TestViewFunctions(unittest.TestCase):
+    # Set up a logged in user for the tests
+    def __init__(self):
+        self.factory = RequestFactory()
+        try:
+            self.user = User.objects.create_user(username='jacob', password='top_secret')
+        except:
+            self.user = Client()
+            self.user.login(username='jacob', password='top_secret')
+
+
+    def test_function(self, name):
+        request = self.factory.get(str(name))
+        request.user = self.user
+        response = eval(name)(request)
+        assert (response.status_code == 200)
+
+
+    def test_administer(self):
+        self.test_function('administer')
+
+
+    def test_create(self):
+        self.test_function('create')
+
+
+    def test_view_all(self):
+        request = self.factory.get('view_all')
+        request.user = self.user
+        response = viewAll(request)
+        assert (response.status_code == 200)
+
+
+def main():
+    myTest = TestViewFunctions()
+    myTest.test_view_all()
+    myTest.test_create()
+    myTest.test_administer()
+
+
+main()
