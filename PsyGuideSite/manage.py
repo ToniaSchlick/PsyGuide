c290a01dd9d6f69ed2e123f31bc409e8af2762f9
puddinbustra <43453214+puddinbustra@users.noreply.github.com>
2019-04-22 21:02:44 -0600

    Update manage.py

--- PsyGuideSite/manage.py
+++ PsyGuideSite/manage.py
@@ -4,7 +4,6 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PsyGuideSite.settings")
-    set DJANGO_SETTINGS_MODULE=mysite.settings
     INSTALLED_APPS = (
 
         'django_extensions'
