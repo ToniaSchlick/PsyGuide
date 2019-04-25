8ce700085f265536377f1ab96ee9030dbab1ad29
Tonia Schlick <tonia.schlick@gmail.com>
2019-04-25 02:10:01 -0600

    trying - remove _init_.py where unneeded

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,23 +1,23 @@
-# #TRAVIS PYTEST EXAMPLE
+#TRAVIS PYTEST EXAMPLE
 
-# # from file import function, for example: from prescriber.admin import adminFunction
-# from prescriber.exampleTest import aFunction
-# from prescriber.views import *
-# from django.utils import unittest
-# from django.test.client import RequestFactory
+# from file import function, for example: from prescriber.admin import adminFunction
+from prescriber.exampleTest import aFunction
+from prescriber.views import *
+from django.utils import unittest
+from django.test.client import RequestFactory
 
 
-# # Note: The name of this file must start with test_, as well as the test functions. This is only because of pytest.
-# ## A dummy template for how to format tests
-# # def test_aFunction():
-# #     assert(someFunction((myInput) == functionOutput))
+# Note: The name of this file must start with test_, as well as the test functions. This is only because of pytest.
+## A dummy template for how to format tests
+# def test_aFunction():
+#     assert(someFunction((myInput) == functionOutput))
 
 
 # def test_example():
 #     assert(aFunction(10) == 10)
 
-# # Testing whether the index function completes as it should
-# def test_index():
-#     f = RequestFactory()
-#     request = self.factory.get('/index')
-#     print("Going through test_index okay")
+# Testing whether the index function completes as it should
+def test_index():
+    f = RequestFactory()
+    request = self.factory.get('/index')
+    print("Going through test_index okay")
