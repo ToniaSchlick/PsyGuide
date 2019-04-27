d9a3f7f9c79be8c6bf2c09a6cae8650474f8dd83
Max W <maxsw16@gmail.com>
2019-04-26 23:03:18 -0600

    Add more Questionnaire tests

--- PsyGuideSite/questionnaire/tests.py
+++ PsyGuideSite/questionnaire/tests.py
@@ -1,8 +1,8 @@
 from django.test import RequestFactory, TestCase
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import AnonymousUser
 from questionnaire.views import *
 from questionnaire.models import *
+from patient.models import Patient
 
 
 class TestViewFunctions(TestCase):
@@ -11,10 +11,16 @@ class TestViewFunctions(TestCase):
         self.factory = RequestFactory()
 
         User = get_user_model()
-        self.user = User.objects.create_user(username='jacob', password='top_secret')
+        self.user = User.objects.create_user(
+            username='jacob',
+            password='top_secret'
+        )
 
         self.qInst = Questionnaire.objects.create(name="Questionnaire 1")
-        self.pInst = Patient.objects.create()
+        self.pInst = Patient.objects.create(
+            first_name="Bob",
+            last_name="Loblaw"
+        )
 
     # ** Utility functions must not have test_ as a prefix
     # This is so they aren't called in error by the TestRunner
@@ -24,41 +30,57 @@ class TestViewFunctions(TestCase):
         response = eval(viewName)(request)
         self.assertEqual(response.status_code, expectedCode)
 
-    def util_test_anon_view(self, viewName, expectedCode, data={}):
-        request = self.factory.get(viewName, data)
-        request.user = AnonymousUser()
+    def util_test_view_post(self, viewName, expectedCode, data={}):
+        request = self.factory.post(viewName, data)
+        request.user = self.user
         response = eval(viewName)(request)
         self.assertEqual(response.status_code, expectedCode)
 
-
-
     def test_administer(self):
-        # No patient of questionnaire
         self.util_test_view('administer', 200)
 
-        # Specify only patient
-        self.util_test_view("administer", 200, {
-            "ppk": self.pInst.pk
-        })
+        # Test with just patient id
+        self.util_test_view('administer', 200, {"ppk": self.pInst.pk})
 
-        # Specify both patient and questionnaire
-        self.util_test_view("administer", 200, {
+        # Test with both patient and questionnaire id
+        self.util_test_view('administer', 200, {
             "ppk": self.pInst.pk,
             "qpk": self.qInst.pk
+        })
+
+        # Test loading of canned response
+        file = open("questionnaire/test_data/questionnaire_response_creation.json", "r")
+        self.util_test_view_post("administer", 302, {
+            "response": file.read(),
+            "qpk": self.qInst.pk,
+            "ppk": self.pInst.pk
         })
 
     def test_view(self):
         self.util_test_view('view', 200)
 
-        self.util_test_view('view', 200, {
-            "qpk": self.qInst.pk
-        })
+        self.util_test_view('view', 200, {"qpk": self.qInst.pk})
 
     def test_viewResponse(self):
         self.util_test_view('view_response', 200)
 
+        self.rInst = QuestionnaireResponse.objects.create(
+            patient=self.pInst,
+            questionnaire=self.qInst,
+        )
+
+        self.util_test_view('view_response', 200, {
+            "qrpk": self.rInst.pk
+        })
+
     def test_create(self):
         self.util_test_view('create', 200)
+
+        # Load test data and submit it
+        file = open("questionnaire/test_data/questionnaire_creation.json", "r")
+        self.util_test_view_post("create", 302, {
+            "questionnaire": file.read()
+        })
 
     def test_viewAll(self):
         self.util_test_view('view_all', 200)
@@ -66,22 +88,17 @@ class TestViewFunctions(TestCase):
     def test_edit(self):
         self.util_test_view('edit', 200)
 
+        self.util_test_view('edit', 200, {"qpk": self.qInst.pk})
+
     def test_delete(self):
         # Delete redirects always with a login
-        self.util_test_view('delete', 302, {
-            "qpk": self.qInst.pk
-        })
+        self.util_test_view('delete', 302)
 
-        # Shows please login else
-        self.util_test_anon_view('delete', 200, {
-            "qpk": self.qInst.pk
-        })
-
-class TestQuestionnaireModels(TestCase):
+class TestQuestionnaire(TestCase):
     def setUp(self):
         self.qInst = Questionnaire.objects.create(name="Questionnaire 1")
 
-    def test_questionnaire_addQuestionSet(self):
+    def test_addQuestionSet(self):
         # New Questionnaire should have no sets
         self.assertEqual(self.qInst.getQuestionSets().count(), 0)
 
@@ -89,29 +106,33 @@ class TestQuestionnaireModels(TestCase):
 
         self.assertEqual(self.qInst.getQuestionSets().count(), 1)
 
-    def test_questionnaire_addScoringFlag(self):
+    def test_addScoringFlag(self):
         self.assertEqual(self.qInst.getScoringFlags().count(), 0)
 
         self.qInst.addScoringFlag("Always True", "1==1", "true")
 
         self.assertEqual(self.qInst.getScoringFlags().count(), 1)
 
-    def test_questionSet_addQuestion(self):
-        # Add a questionSet to add questions to
-        set = self.qInst.addQuestionSet(0, "Set 1", True)
+class TestQuestionSet(TestCase):
+    def setUp(self):
+        self.qInst = Questionnaire.objects.create(name="Questionnaire 1")
+        self.qSet = self.qInst.addQuestionSet(0, "boring topic", False)
 
-        self.assertEqual(set.getQuestions().count(), 0)
+    def test_addQuestion(self):
+        self.assertEqual(self.qSet.getQuestions().count(), 0)
 
-        set.addQuestion(0, "Question 1")
+        self.qSet.addQuestion(0, "Question 1")
 
-        self.assertEqual(set.getQuestions().count(), 1)
+        self.assertEqual(self.qSet.getQuestions().count(), 1)
 
     def test_questionSet_addAnswer(self):
-        # Add a questionSet to add questions to
-        set = self.qInst.addQuestionSet(0, "Set 1", True)
+        self.assertEqual(self.qSet.getAnswers().count(), 0)
 
-        self.assertEqual(set.getAnswers().count(), 0)
+        self.qSet.addAnswer(0, "Answer 1")
 
-        set.addAnswer(0, "Answer 1")
+        self.assertEqual(self.qSet.getAnswers().count(), 1)
 
-        self.assertEqual(set.getAnswers().count(), 1)
+class TestQuestionnaireResponse(TestCase):
+    def setUp(self):
+        self.qInst = Questionnaire.objects.create(name="Questionnaire 1")
+        self.qSet = self.qInst.addQuestionSet(0, "boring topic", False)
